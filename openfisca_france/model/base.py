73d08434d68fc02c48ddd2715e13cd3a22911eb9
Florian Pagnoux <Florian.Pagnoux@gmail.com>
2015-10-09 15:42:37 +0200

    Rename make_reference_formula_decorator => make_law_variable_decorator

--- openfisca_france/model/base.py
+++ openfisca_france/model/base.py
@@ -8,7 +8,7 @@ from openfisca_core.columns import (AgeCol, BoolCol, build_column, DateCol, Enum
 from openfisca_core.enumerations import Enum
 from openfisca_core.formulas import (calculate_output_add, calculate_output_add_divide, calculate_output_divide,
     dated_function, DatedFormulaColumn, EntityToPersonColumn, last_duration_last_value,
-    make_reference_formula_decorator, missing_value, PersonToEntityColumn, reference_input_variable,
+    make_law_variable_decorator, missing_value, PersonToEntityColumn, reference_input_variable,
     requested_period_added_value, requested_period_default_value, requested_period_last_value,
     set_input_dispatch_by_period, set_input_divide_by_period, SimpleFormulaColumn)
 
