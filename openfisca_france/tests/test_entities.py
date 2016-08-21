aae10afd934a74e9cd36bac1f91e154573ba58d2
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-08-21 23:17:08 +0200

    Add entities manipulation tests

--- openfisca_france/tests/test_entities.py
+++ openfisca_france/tests/test_entities.py
@@ -1,24 +1,32 @@
+from copy import deepcopy
+
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 from openfisca_core.variables import Variable
 from openfisca_core.columns import FloatCol
+from openfisca_core.tools import assert_near, assert_equal
+
 from openfisca_france.scenarios import Scenario
-from openfisca_france.entities import entities, Familles, FoyersFiscaux
+from openfisca_france.entities import entities, Individus, Familles, FoyersFiscaux, Menages, PARENT, ENFANT, CONJOINT, PERSONNE_DE_REFERENCE
 
 class af(Variable):
     column = FloatCol
     entity_class = Familles
     label = u"Allocations familiales"
 
+class salaire(Variable):
+    column = FloatCol
+    entity_class = Individus
+    label = u"Salaire"
+
 class DummyTaxBenefitSystem(TaxBenefitSystem):
     def __init__(self):
         TaxBenefitSystem.__init__(self, entities)
         self.Scenario = Scenario
-        self.add_variables(af)
+        self.add_variables(af, salaire)
 
 tbs = DummyTaxBenefitSystem()
 
-
-test_case = {
+TEST_CASE = {
     'individus': [{'id': 'ind0'}, {'id': 'ind1'}, {'id': 'ind2'}, {'id': 'ind3'},{'id': 'ind4'}, {'id': 'ind5'}],
     'familles': [
         {'enfants': ['ind2', 'ind3'], 'parents': ['ind0', 'ind1']},
@@ -34,8 +42,58 @@ test_case = {
         ],
     }
 
-simulation = tbs.new_scenario().init_from_test_case(
-    period = 2013,
-    test_case = test_case
+def new_simulation(test_case):
+    return tbs.new_scenario().init_from_test_case(
+        period = 2013,
+        test_case = test_case
     ).new_simulation()
 
+def test_entities_id_and_role_columns():
+
+    simulation = new_simulation(TEST_CASE)
+
+    id_famille_column_name = tbs.get_entity_index_column_name(Familles)
+    role_in_famille_column_name = tbs.get_entity_role_column_name(Familles)
+    postion_in_famille_column_name = tbs.get_entity_position_column_name(Familles)
+
+    assert_near(simulation.calculate(id_famille_column_name), [0,0,0,0,1,1])
+    assert_equal(
+        simulation.calculate(role_in_famille_column_name),
+        [PARENT, PARENT, ENFANT, ENFANT, PARENT, ENFANT]
+        )
+    assert_near(simulation.calculate(postion_in_famille_column_name), [0,1,2,3,0,1])
+
+    role_in_menage_column_name = tbs.get_entity_role_column_name(Menages)
+
+    assert_equal(
+        simulation.calculate(role_in_menage_column_name),
+        [PERSONNE_DE_REFERENCE, CONJOINT, ENFANT, ENFANT, PERSONNE_DE_REFERENCE, ENFANT]
+        )
+
+def test_projection():
+    test_case = deepcopy(TEST_CASE)
+    test_case['familles'][0]['af'] = 20000
+
+    simulation = new_simulation(test_case)
+
+    af = simulation.calculate('af')
+    af_projete = simulation.project_on_persons(af, entity = Familles)
+
+    assert_near(af_projete, [20000, 20000, 20000, 20000, 0, 0])
+
+def test_transpose_to_entity():
+    test_case = deepcopy(TEST_CASE)
+    test_case['familles'][0]['af'] = 20000
+    test_case['familles'][1]['af'] = 10000
+    test_case['foyers_fiscaux'] = [
+        TEST_CASE['foyers_fiscaux'][0],
+        {'declarants': ['ind4']},
+        {'declarants': ['ind5']}
+        ]
+
+    simulation = new_simulation(test_case)
+
+    af = simulation.calculate('af')
+    af_foyer_fiscal = simulation.transpose_to_entity(af, target_entity = FoyersFiscaux, origin_entity = Familles)
+
+    assert_near(af_foyer_fiscal, [20000, 10000, 0])
